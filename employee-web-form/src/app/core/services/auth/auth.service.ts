a2ac14d1b296e476281990734172db3635580507
JoaoBreder <joao.g.breder@gmail.com>
2023-03-06 20:09:56 -0300

    Botão de logout

--- employee-web-form/src/app/core/services/auth/auth.service.ts
+++ employee-web-form/src/app/core/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from 'src/app/shared/models/user';
+import { Router } from '@angular/router';
 
 
 @Injectable({ providedIn: 'root' })
@@ -9,7 +10,10 @@ export class AuthService {
   private userSubject: BehaviorSubject<User | null>;
   public user: Observable<User | null>;
 
-  constructor(public angularFire: AngularFireAuth) {
+  constructor(
+    private angularFire: AngularFireAuth,
+    private router: Router
+  ) {
     this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
     this.user = this.userSubject.asObservable();
   }
@@ -33,7 +37,10 @@ export class AuthService {
   }
 
   async signOut() {
+    localStorage.clear();
+    this.userSubject.next(null);
     await this.angularFire.signOut();
+    this.router.navigateByUrl('/login');
   }
 
 }
--- employee-web-form/src/app/pages/home/home.component.ts
+++ employee-web-form/src/app/pages/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  loading: boolean = false;
 
-  constructor() {
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  }
-
-  ngOnInit(): void {
-
+  async logout() {
+    this.loading = true;
+    setTimeout(async () => {
+      this.loading = true;
+      await this.authService.signOut();
+    }, 500);
   }
 }
